08579029c668bdd56dcc25d4c7e4fa85e2a54b35
juaner1993 <1003591756@qq.com>
2022-01-07 13:28:25 +0800

    修改

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
-        System.out.println("HelloJava!");
+        System.out.println("Hecfghjklvbnm,.ghjkla!");
         System.out.println("HelloHaha!");
     }
 }
